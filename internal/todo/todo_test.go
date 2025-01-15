cafc178a68ba833e294ced10552064175f4b2c47
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-01-15 21:59:11 +0000

    add first level of test

--- internal/todo/todo_test.go
+++ internal/todo/todo_test.go
@@ -1,32 +1,55 @@
-package todo
+package todo_test
 
 import (
+	"my-first-api/internal/todo"
 	"reflect"
 	"testing"
 )
 
 func TestService_Search(t *testing.T) {
-	type fields struct {
-		todos []Item
-	}
-	type args struct {
-		query string
-	}
+
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   []string
+		name           string
+		todosToAdd     []string
+		query          string
+		expectedResult []string
 	}{
-		// TODO: Add test cases.
+		{
+			name:           "given a todo of shop and a search of sh, I should get shop back",
+			todosToAdd:     []string{"shop"},
+			query:          "sh",
+			expectedResult: []string{"shop"},
+		},
+		{
+			name:           "still returns shop even if the case doesn't match",
+			todosToAdd:     []string{"Shopping"},
+			query:          "sh",
+			expectedResult: []string{"Shopping"},
+		},
+		{
+			name:           "spaces",
+			todosToAdd:     []string{"go Shopping"},
+			query:          "go",
+			expectedResult: []string{"go Shopping"},
+		},
+		{
+			name:           "space at the start of a word",
+			todosToAdd:     []string{" Space at the beginning"},
+			query:          "space",
+			expectedResult: []string{" Space at the beginning"},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			svc := &Service{
-				todos: tt.fields.todos,
+			svc := todo.NewService()
+			for _, toAdd := range tt.todosToAdd {
+				err := svc.Add(toAdd)
+				if err != nil {
+					t.Error(err)
+				}
 			}
-			if got := svc.Search(tt.args.query); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Search() = %v, want %v", got, tt.want)
+			if got := svc.Search(tt.query); !reflect.DeepEqual(got, tt.expectedResult) {
+				t.Errorf("Search() = %v, want %v", got, tt.expectedResult)
 			}
 		})
 	}
